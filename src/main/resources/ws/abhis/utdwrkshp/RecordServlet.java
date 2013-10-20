bb021bcea1bc003c62e94dd09033773fbb758a2a
UTD <utd@utd-VirtualBox.(none)>
2013-10-20 02:32:40 -0500

    Added - Comments written to file (comments.txt). Error is BufferedWriter Object name used is incorrect. This was not giving an error in eclipse!

--- src/main/resources/ws/abhis/utdwrkshp/RecordServlet.java
+++ src/main/resources/ws/abhis/utdwrkshp/RecordServlet.java
@@ -1,5 +1,8 @@
 package main.webapp;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
 import javax.servlet.http.HttpServlet;
 
 public class RecordServlet extends HttpServlet{
@@ -9,5 +12,8 @@ public class RecordServlet extends HttpServlet{
         String name = request.getParameter("name");
         String email = request.getParameter("email");
         String comment = request.getParameter("comment");
+        
+        BufferedWriter writer = new BufferedWriter(new FileWriter(File("comments.txt")));
+        bufferedWriter.append(name + " " + email + " " + comment);
 	}
 }
